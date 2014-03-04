8ab1b703e2c551ada6c346bbdcc0a40597a9a106
unknown <rhiggins@SGH023TLYQ.ad.sl.nsw.gov.au>
2014-03-04 12:19:27 +1100

    Initial build

--- scripts/derby_custom.js
+++ scripts/derby_custom.js
@@ -151,6 +151,12 @@
 		  },"slow");
 
 		});
+		
+		// idle timer function
+		$( document ).idleTimer( 2*60*1000 );
+		$( document ).on( "idle.idleTimer", function(){
+			 document.location.href = 'index.html';
+		});
 	
 		(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
 				
